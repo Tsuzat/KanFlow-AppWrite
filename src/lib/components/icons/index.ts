f56303013c80640695b7f04e9c39fa350184d8bf
Tsuzat <dev.tsuzat@gmail.com>
2025-09-11 11:26:34 +0530

    Add GitHub and Google icon components

---
+++ src/lib/components/icons/index.ts
@@ -0,0 +1,10 @@
+import { Loader, Mail } from '@lucide/svelte/icons';
+import Google from './google.svelte';
+import Github from './github.svelte';
+
+export default {
+	email: Mail,
+	google: Google,
+	github: Github,
+	loader: Loader
+};
