e20e0892b30ab15c6846eade2832465825fe8d5f
Tsuzat <dev.tsuzat@gmail.com>
2025-09-11 20:26:06 +0530

    Add node adapter and mdsvex for Markdown support

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+import { mdsvex } from 'mdsvex';
+
+/** @type {import('mdsvex').MdsvexOptions} */
+const mdsvexConfigs = {
+	extensions: ['.md', '.svx']
+};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	preprocess: [vitePreprocess(), mdsvex(mdsvexConfigs)],
+
+	kit: { adapter: adapter() },
+	extensions: ['.svelte', '.svx', '.md']
 };
 
 export default config;
