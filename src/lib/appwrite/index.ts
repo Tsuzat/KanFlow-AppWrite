04e09cc39e7d70988aeca1d6ca476b71a88f1bef
Tsuzat <dev.tsuzat@gmail.com>
2025-09-11 12:17:05 +0530

    Add getUserInitals function to extract initials from username

--- src/lib/appwrite/index.ts
+++ src/lib/appwrite/index.ts
@@ -6,3 +6,18 @@ export const client = new Client();
 client.setEndpoint(PUBLIC_APPWRITE_ENDPOINT).setProject(PUBLIC_APPWRITE_PROJECT_ID);
 export const account = new Account(client);
 export { ID } from 'appwrite';
+
+export const getUserInitals = (userName: string) => {
+	try {
+		const words = userName.split(' ');
+		if (words.length > 1) {
+			return words[0][0] + words[1][0];
+		} else if (words.length === 1 && words[0].length > 1) {
+			return words[0][0] + words[0][1];
+		} else {
+			return words[0][0];
+		}
+	} catch {
+		return '?';
+	}
+};
