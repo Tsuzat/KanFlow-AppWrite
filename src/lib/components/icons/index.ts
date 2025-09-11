e46c2851ce22d86d0e89b46169e47bd05295a012
Tsuzat <dev.tsuzat@gmail.com>
2025-09-11 12:17:21 +0530

    Add additional icons (Sun, MoonStar, Plus) to the Icons export

--- src/lib/components/icons/index.ts
+++ src/lib/components/icons/index.ts
@@ -1,10 +1,15 @@
-import { Loader, Mail } from '@lucide/svelte/icons';
+import { Loader, Mail, Sun, MoonStar, Plus } from '@lucide/svelte/icons';
 import Google from './google.svelte';
 import Github from './github.svelte';
 
-export default {
+const Icons = {
 	email: Mail,
 	google: Google,
 	github: Github,
-	loader: Loader
+	loader: Loader,
+	sun: Sun,
+	moon: MoonStar,
+	add: Plus
 };
+
+export default Icons;
