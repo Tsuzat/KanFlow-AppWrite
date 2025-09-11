fc14566d1a1b7801f1f155bd4f550769be81172a
Tsuzat <dev.tsuzat@gmail.com>
2025-09-11 11:26:56 +0530

    Add failed sign-in page with error message and home button

---
+++ src/routes/(auth)/failed/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<p class="mt-8 text-center">
+	Sign In Attempt Failed. Please try again or contact support.
+	<Button variant="link" href="/">Go to Home</Button>
+</p>
