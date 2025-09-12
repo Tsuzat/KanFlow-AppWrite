ef283453529903f4ecf1ee2506e980472506d934
Tsuzat <dev.tsuzat@gmail.com>
2025-09-12 23:32:38 +0530

    refactor(appwrite): initialize client with endpoint and project id
    Move client configuration to initialization and reorder exports for better readability

--- src/lib/appwrite/index.ts
+++ src/lib/appwrite/index.ts
@@ -1,11 +1,12 @@
 import { PUBLIC_APPWRITE_ENDPOINT, PUBLIC_APPWRITE_PROJECT_ID } from '$env/static/public';
 import { Client, Account, Avatars, TablesDB } from 'appwrite';
-
-export const client = new Client();
-
-client.setEndpoint(PUBLIC_APPWRITE_ENDPOINT).setProject(PUBLIC_APPWRITE_PROJECT_ID);
-export const account = new Account(client);
 export { ID } from 'appwrite';
+
+export const client = new Client()
+	.setEndpoint(PUBLIC_APPWRITE_ENDPOINT)
+	.setProject(PUBLIC_APPWRITE_PROJECT_ID);
+
+export const account = new Account(client);
 export const avatars = new Avatars(client);
 export const databases = new TablesDB(client);
 
