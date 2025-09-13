624debeac2b65edc53e2265ab26ed35f45f5451b
Tsuzat <dev.tsuzat@gmail.com>
2025-09-13 11:10:29 +0530

    feat(components): add priority selector component for task management

---
+++ src/lib/components/custom/utils/PrioritySelector.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { Priority } from '$lib/appwrite/db/tasks.svelte';
+	import * as Select from '$lib/components/ui/select/index.js';
+	interface Props {
+		priority?: Priority;
+	}
+	let { priority = $bindable() }: Props = $props();
+	const priorities = [
+		{ value: Priority.Low, label: 'Low' },
+		{ value: Priority.Medium, label: 'Medium' },
+		{ value: Priority.High, label: 'High' }
+	];
+</script>
+
+<Select.Root type="single" bind:value={priority}>
+	<Select.Trigger class="w-fit capitalize">
+		{priority ?? 'Select Priority'}
+	</Select.Trigger>
+	<Select.Content>
+		<Select.Group>
+			<Select.Label>Priorities</Select.Label>
+			{#each priorities as pr (pr.value)}
+				<Select.Item value={pr.value}>
+					{pr.label}
+				</Select.Item>
+			{/each}
+		</Select.Group>
+	</Select.Content>
+</Select.Root>
